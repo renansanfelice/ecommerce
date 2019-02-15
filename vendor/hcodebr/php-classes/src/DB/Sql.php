dfe3f4d9b5032ea7bccdf68ce1be65da8f344090
Renan C Sanfelice <renan@emporiumdigital.net>
2019-02-15 15:45:49 -0200

    Aula 104 - acessos admin e login

--- vendor/hcodebr/php-classes/src/DB/Sql.php
+++ vendor/hcodebr/php-classes/src/DB/Sql.php
@@ -13,47 +13,37 @@ class Sql {
 
 	public function __construct()
 	{
-
 		$this->conn = new \PDO(
 			"mysql:dbname=".Sql::DBNAME.";host=".Sql::HOSTNAME,
 			Sql::USERNAME,
 			Sql::PASSWORD
 		);
-
 	}
 
 	private function setParams($statement, $parameters = array())
 	{
-
 		foreach ($parameters as $key => $value) {
 
 			$this->bindParam($statement, $key, $value);
-
 		}
-
 	}
 
 	private function bindParam($statement, $key, $value)
 	{
-
 		$statement->bindParam($key, $value);
-
 	}
 
 	public function query($rawQuery, $params = array())
 	{
-
 		$stmt = $this->conn->prepare($rawQuery);
 
 		$this->setParams($stmt, $params);
 
 		$stmt->execute();
-
 	}
 
 	public function select($rawQuery, $params = array()):array
 	{
-
 		$stmt = $this->conn->prepare($rawQuery);
 
 		$this->setParams($stmt, $params);
@@ -61,7 +51,6 @@ class Sql {
 		$stmt->execute();
 
 		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
-
 	}
 
 }
