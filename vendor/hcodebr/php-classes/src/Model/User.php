1097a108d87d7a7df53df66a81faf65a8d14a421
Renan C Sanfelice <renan@emporiumdigital.net>
2019-03-04 17:07:00 -0300

    Aula de Esqueci minha senha

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -4,9 +4,14 @@ namespace Hcode\Model;
 
 use \Hcode\DB\Sql;
 use \Hcode\Model;
+use \Hcode\Mailer;
 
 class User extends Model {
   const SESSION = "User";
+  const SECRET = "EmporiumDigital_";
+  const CHAVE = "RECUPERAR_SENHAS";
+  const ENCRYPT_METHOD = "AES-256-CBC";
+
 
     public static function login($login, $password)
     {
@@ -124,6 +129,142 @@ class User extends Model {
             ));
     }
 
-}
+    public static function getForgot($email)
+  	{
+      $sql = new Sql();
 
- ?>
+  		$results = $sql->select("
+  			SELECT *
+  			FROM tb_persons a
+  			INNER JOIN tb_users b USING(idperson)
+  			WHERE a.desemail = :email;
+  		", array(
+  			":email"=>$email
+  		));
+
+  		if (count($results) === 0)
+  		{
+  			throw new \Exception("Não foi possível recuperar a senha.");
+
+  		}
+  		else
+  		{
+
+  			$data = $results[0];
+
+  			$results2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", array(
+  				":iduser"=>$data["iduser"],
+  				":desip"=>$_SERVER["REMOTE_ADDR"]
+  			));
+
+  			if (count($results2) === 0)
+  			{
+
+  				throw new \Exception("Não foi possível recuperar a senha");
+
+  			}
+  			else
+  			{
+
+  				$dataRecovery = $results2[0];
+          $key = hash('sha256', User::SECRET);
+          $iv = substr(hash('sha256', User::CHAVE), 0, 16);
+
+  				//$code = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, User::SECRET, $dataRecovery["idrecovery"], MCRYPT_MODE_ECB));
+          $code = base64_encode(openssl_encrypt($dataRecovery["idrecovery"], User::ENCRYPT_METHOD, $key, 0, $iv));
+
+					$link = "http://www.hloja.com.br/admin/forgot/reset?code=$code";
+
+  				$mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir Senha da Emporium Digital Loja", "forgot", array(
+  					"name"=>$data["desperson"],
+  					"link"=>$link
+  				));
+
+  				$mailer->send();
+
+  				return $data;
+
+  			}
+      }
+    }
+
+    public static function validForgotDecrypt($code)
+    {
+
+      $key = hash('sha256', User::SECRET);
+      $iv = substr(hash('sha256', User::CHAVE), 0, 16);
+
+        $idrecovery = openssl_decrypt(base64_decode($code), User::ENCRYPT_METHOD, $key, 0, $iv);
+
+        $sql = new Sql();
+
+        $results = $sql->select("
+                      SELECT *
+                      FROM tb_userspasswordsrecoveries a
+                      INNER JOIN tb_users b USING(iduser)
+                      INNER JOIN tb_persons c USING(idperson)
+                      WHERE a.idrecovery = :idrecovery
+                      AND a.dtrecovery IS null
+                      AND DATE_aDD(a.dtregister, INTERVAL 1 HOUR) >= NOW();
+                      ", array(
+                              ":idrecovery"=>$idrecovery
+                      ));
+        if (count($results) === 0)
+        {
+            throw new \Exception("Não foi possível recuperar a senha.");
+        } else {
+            return $results[0];
+        }
+    }
+
+    public static function setForgotUsed($idrecovery)
+    {
+        $sql = new Sql();
+
+        $sql->query("UPDATE tb_userspasswordsrecoveries SET dtrecovery = now() WHERE idrecovery = :idrecovery",
+        array(
+              ":idrecovery"=>$idrecovery
+        ));
+    }
+    public function setPassword($password)
+    {
+        $sql = new Sql();
+
+        $sql->query("UPDATE tb_users SET despassword = :password WHERE iduser = :iduser",
+          array(
+            ":password"=>$password,
+            ":iduser"=>$this->getiduser()
+          ));
+    }
+  }
+
+/*
+function encrypt_decrypt($action, $string)
+{
+    /* =================================================
+     * ENCRYPTION-DECRYPTION
+     * =================================================
+     * ENCRYPTION: encrypt_decrypt('encrypt', $string);
+     * DECRYPTION: encrypt_decrypt('decrypt', $string) ;
+     */
+     /*
+    $output = false;
+    $encrypt_method = "AES-256-CBC";
+    $secret_key = 'WS-SERVICE-KEY';
+    $secret_iv = 'WS-SERVICE-VALUE';
+    // hash
+    $key = hash('sha256', $secret_key);
+    // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+    $iv = substr(hash('sha256', $secret_iv), 0, 16);
+    if ($action == 'encrypt') {
+        $output = base64_encode(openssl_encrypt($string, $encrypt_method, $key, 0, $iv));
+    } else {
+        if ($action == 'decrypt') {
+            $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+        }
+    }
+    return $output;
+}
+*/
+
+?>
