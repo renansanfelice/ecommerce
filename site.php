50168f4951eb260323cb7faf37b33aae6e93c1fa
Renan C Sanfelice <renan@emporiumdigital.net>
2019-04-16 09:01:59 -0300

    Aula 113 - Detalhes dos produtos site

--- site.php
+++ site.php
@@ -14,4 +14,18 @@ $app->get('/', function() {
 
 });
 
+$app->get('/products/:desurl', function($desurl){
+
+	$product = new Product();
+
+	$product->getFromUrl($desurl);
+
+	$page = new Page();
+
+	$page->setTpl("product-detail", [
+		'product'=>$product->getValues(),
+		'categories'=>$product->getCategories()
+	]);
+
+})
 ?>
--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -87,7 +87,7 @@ class Product extends Model {
         return $this->setdesphoto($url);
     }
 
-    public function getVAlues() 
+    public function getValues() 
     {
         $this->checkPhoto();
 
@@ -137,5 +137,30 @@ class Product extends Model {
 
     }
 
-  }
+    public function getFromUrl($desurl)
+    {    
+
+        $sql = new Sql();
+
+        $rows = $sql->select("SELECT * FROM tb_products WHERE desurl = :desurl LIMIT 1", [
+            'desurl'=>$desurl
+        ]);
+
+        $this->setData($rows[0]);
+    }
+
+    public function getCategories() 
+    {
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_categories a 
+                        INNER JOIN tb_productscategories b 
+                        ON a.idcategory = b.idcategory 
+                        WHERE b.idproduct = :idproduct", 
+                        [
+                            ':idproduct'=>$this->getidproduct()
+                        ]);
+    }
+}
+
 ?>
